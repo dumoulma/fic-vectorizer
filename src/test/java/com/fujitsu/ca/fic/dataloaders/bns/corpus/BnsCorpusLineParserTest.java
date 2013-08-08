a3a20eea01f6d1d953c633f61ade3439bbae4ab4
dumoulma <mathieu.dumoulin@gmail.com>
2013-08-08 11:46:10 -0400

    updated package name for test of hdfsLoader

--- src/test/java/com/fujitsu/ca/fic/dataloaders/bns/corpus/BnsCorpusLineParserTest.java
+++ src/test/java/com/fujitsu/ca/fic/dataloaders/bns/corpus/BnsCorpusLineParserTest.java
@@ -1,6 +1,21 @@
 package com.fujitsu.ca.fic.dataloaders.bns.corpus;
 
+import java.util.List;
+
+import org.apache.mahout.math.Vector;
+import org.junit.Test;
+
+import com.fujitsu.ca.fic.dataloaders.LineParser;
+import com.fujitsu.ca.fic.exceptions.IncorrectLineFormatException;
+import com.google.common.collect.Lists;
 
 public class BnsCorpusLineParserTest {
+    private static String pigOutputFormatOK = "27677.txt,{(27677.txt,1,blue,blue,0.28095,13),(27677.txt,1,green,green,0.22829,213),(27677.txt,1,yellow,yellow,1.98149,402)}";
+    List<String> tokenIndexList = Lists.newArrayList("blue", "green", "red", "yellow", "orange");
 
+    @Test
+    public void parseCorrectlyFormatterLineDoesntThrowException() throws IncorrectLineFormatException {
+        LineParser<Vector> parser = new BnsCorpusLineParser(tokenIndexList);
+        parser.parseFields(pigOutputFormatOK);
+    }
 }
--- src/test/java/com/fujitsu/ca/fic/dataloaders/hdfs/HdfsCorpusLoaderTest.java
+++ src/test/java/com/fujitsu/ca/fic/dataloaders/hdfs/HdfsCorpusLoaderTest.java
@@ -1,4 +1,4 @@
-package com.fujitsu.ca.fic.dataloaders.hadoop;
+package com.fujitsu.ca.fic.dataloaders.hdfs;
 
 import java.io.IOException;
 
