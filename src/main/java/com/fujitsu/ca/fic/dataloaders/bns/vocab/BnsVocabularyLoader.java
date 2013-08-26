6cfba76b2b65a8b235b433007533e8d588f74838
dumoulma <mathieu.dumoulin@gmail.com>
2013-08-26 16:47:32 -0400

    removed @see from javadoc

--- src/main/java/com/fujitsu/ca/fic/dataloaders/bns/vocab/BnsVocabularyLoader.java
+++ src/main/java/com/fujitsu/ca/fic/dataloaders/bns/vocab/BnsVocabularyLoader.java
@@ -16,31 +16,34 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class BnsVocabularyLoader implements VocabularyLoader {
-    private static final Logger LOG = LoggerFactory.getLogger(BnsVocabularyLoader.class);
+	private static final Logger LOG = LoggerFactory
+			.getLogger(BnsVocabularyLoader.class);
 
-    private static final Map<String, Double> BNS_MAP = Maps.newHashMap();
-    private static final List<String> TOKEN_INDEX = Lists.newArrayList();
+	private static final Map<String, Double> BNS_MAP = Maps.newHashMap();
+	private static final List<String> TOKEN_INDEX = Lists.newArrayList();
 
-    /**
-     * Reads from HDFS a list of tokens with their associated BNS term weights. Returns the list of tokens. We also keep a dictionary with
-     * the tokens and their BNS weights. The index in this list is the position of the word in the vector space representation.
-     * 
-     * <p>
-     * NOTE: This list will not likely be larger than 5k to 25k entries, it is safe to keep in memory. The current default targetted
-     * vocabulary size is around 5000 words.
-     * </p>
-     * 
-     * 
-     * @see com.fujitsu.ca.fic.dataloaders.bnsvocab.VocabularyLoader#loadFromText(org.apache.hadoop.conf.Configuration, java.lang.String)
-     */
-    @Override
-    public List<String> loadFromText(Configuration conf, String pathName) throws IOException {
-        HdfsCorpusLoader<Pair<String, Double>> corpusLoader = BnsCorpusFactory.createHdfsVocabLoader(conf, pathName);
-        for (Pair<String, Double> nextPair : corpusLoader) {
-            LOG.debug("Next Pair: " + nextPair);
-            BNS_MAP.put(nextPair.first, nextPair.second);
-            TOKEN_INDEX.add(nextPair.first);
-        }
-        return TOKEN_INDEX;
-    }
+	/**
+	 * Reads from HDFS a list of tokens with their associated BNS term weights.
+	 * Returns the list of tokens. We also keep a dictionary with the tokens and
+	 * their BNS weights. The index in this list is the position of the word in
+	 * the vector space representation.
+	 * 
+	 * <p>
+	 * NOTE: This list will not likely be larger than 5k to 25k entries, it is
+	 * safe to keep in memory. The current default targetted vocabulary size is
+	 * around 5000 words.
+	 * </p>
+	 */
+	@Override
+	public List<String> loadFromText(Configuration conf, String pathName)
+			throws IOException {
+		HdfsCorpusLoader<Pair<String, Double>> corpusLoader = BnsCorpusFactory
+				.createHdfsVocabLoader(conf, pathName);
+		for (Pair<String, Double> nextPair : corpusLoader) {
+			LOG.debug("Next Pair: " + nextPair);
+			BNS_MAP.put(nextPair.first, nextPair.second);
+			TOKEN_INDEX.add(nextPair.first);
+		}
+		return TOKEN_INDEX;
+	}
 }
